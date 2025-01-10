8d59aa3b53ef92a815400b19151936b41bcd006d
unknown <wahickssware84@gmail.com>
2025-01-09 19:51:29 -0600

    Update architecture.

--- src/Brytenwealda.java
+++ src/Brytenwealda.java
@@ -1,7 +1,26 @@
 package brytenwealda;
 
+import java.util.Scanner;
+
+/**
+ * @param 
+ * @author wahic
+ */
 public class Brytenwealda {
-  public static void main(String[] args) {
-    
-  }
+
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner in = new Scanner(System.in);
+        // Introduction.
+        System.out.println("\nWelcome to Brytenwealda!\n");
+        displayWindow();
+        System.out.println("\n\tSelect your faction:\n");
+        displayWindow();
+
+    }
+
+    public static void displayWindow() {
+        System.out.println("*****************************************");
+    }
+
 }
--- src/Combat.java
+++ src/Combat.java
@@ -1,9 +1,12 @@
 package brytenwealda;
+//import java.util.Scanner;
 
+/**
+ * @param 
+ * @author wahic
+ */
 public class Combat {
-  double rateOfAttrition = 1.0;
-  double playerAttrition = 1.0;
-  double enemyAttrition = 1.0;
 
-  
+  double rateOfAttrition = 1.0;
+  double playerAttrition, enemyAttrition = rateOfAttrition;
 }
